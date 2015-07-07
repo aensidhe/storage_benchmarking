489e7a34a44383665f436672c730409354b1eb12
Anatoly Popov <aensidhe@users.noreply.github.com>
2015-07-07 16:27:44 +0300

    Removed async.

--- test.js
+++ test.js
@@ -9,40 +9,41 @@ function format_ms(hrtime) {
 	return (hrtime[0] * 1e9 + hrtime[1]) / 1e6;
 }
 
-function test_storage(storage, num_count, next) {
-	var start = process.hrtime();
-	async.times(
-		num_count,
-		function(n, next) {
-			storage.read_one_random_record(next);
-		},
-		function(err) {
-			var stop = process.hrtime(start);
-			console.log("Test finished: %d ops, %d ms, %d ops/sec", num_count, format_ms(stop), num_count * 1000 / format_ms(stop));
+function test_storage(options) {
+	if (options.index >= options.iteration_count)
+	{
+		var stop = process.hrtime(options.start);
+		console.log("Test finished: %d ops, %d ms, %d ops/sec",
+			options.iteration_count, 
+			format_ms(stop), 
+			options.iteration_count * 1000 / format_ms(stop));
 			
-			storage.teardown();
-			console.log();
-			next();
-		}
-	);
+		options.storage.teardown();
+		console.log();
+		options.done();
+		return;
+	}
+	
+	options.index++;
+	options.storage.read_one_random_record(function() { test_storage(options); });
 }
 
 function simple_test(name, storage_creator, num_count, record_count, next) {
 	var start = process.hrtime();
 	console.log("Test '" + name + "' started");
 	
-	var storage = storage_creator(record_count, function (err, s) { 
+	storage_creator(record_count, function (err, storage) { 
 		console.log("Storage created in %d ms", format_ms(process.hrtime(start)));
 		if (err)
 		{
 			console.log(err);
 			return;
 		}	
-		test_storage(s, num_count, next);
+		test_storage({ storage: storage, iteration_count: num_count, done: next, start: process.hrtime(), index: 0 });
 	});
 };
 
-var num = 5e2;
+var num = 5e5;
 var record_num = 1e6;
 
 var runner = async.seq(
