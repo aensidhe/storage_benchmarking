664746f34a2dcd20ce8ed2a1bae386abc109b2fe
Anatoly Popov <aensidhe@users.noreply.github.com>
2015-07-08 00:01:07 +0300

    Finally working variant. Strange lib.

--- data/riak.js
+++ data/riak.js
@@ -3,38 +3,40 @@ var util = require("./util.js");
 var client = new Riak.Client(["127.0.0.1:8087"]);
 var semaphore = require("semaphore")(10);
 
+var count = 0;
+var max_count = util.max_record_count;
+
+function create_riak_object(i) {
+    var riakObj = new Riak.Commands.KV.RiakObject();
+    riakObj.setContentType("text/plain");
+    riakObj.setValue(util.random_string(1024));
+    return { bucket: "storage_test", key: "key" + i, value: riakObj };
+}
+
+function insert_value() {
+    client.storeValue(create_riak_object(count), function(err, rslt) {
+        if (err) {
+            throw "Err at count = " + count + ", " + err;
+        }
+        
+        if (++count >= max_count) {
+            client.shutdown(function() { util.done(); });
+            return;
+        }
+        
+        if (count % util.record_per_iteration == 0) {
+            console.log("Inserted " + count + ".");
+        }
+        
+        insert_value();
+    });
+}
+
 client.ping(function(err) {
     if (err) {
         util.done(err);
         return;
     }
-    var count = 0;
-    var max_count = 1000;
-    for (var i = 0; i < max_count; i++) {
-        semaphore.take(function () {
-            var riakObj = new Riak.Commands.KV.RiakObject();
-            riakObj.setContentType("text/plain");
-            riakObj.setValue(util.random_string(1024));
-            var options = { bucket: "storage_test", key: "key" + i, value: riakObj };            
-            client.storeValue(options, function(err, rslt){
-                semaphore.leave();                
-                if (err)
-                {
-                    throw "Error at i = " + i + err;
-                }
-                
-                if (++count >= max_count) {
-                    client.shutdown(function (state) {
-                        if (state === Riak.Cluster.State.SHUTDOWN) {
-                            process.exit();
-                        }
-                    });
-                }
-                
-                if (count % 10 == 0) {
-                    console.log("Inserted " + count + " records. " + JSON.stringify(rslt));
-                }
-            });
-        });
-    }
 });
+
+insert_value();
